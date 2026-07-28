b02f68b198325e29a01e43deec4b4f84b0a3f690
SoniVega53 <svegam@miumg.edu.gt>
2026-07-28 09:18:41 -0600

    feat: control loader page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,28 @@
-import { Component } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
+import { Component, Inject, OnInit, PLATFORM_ID } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { LoaderComponent } from "./shared/loader/loader.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, LoaderComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
-export class AppComponent {
-  title = 'frontend-equipo-bravo-analisis-sistema-II';
+export class AppComponent implements OnInit {
+  isCheckingAuth = true;
+  isBrowser = false;
+
+  constructor(@Inject(PLATFORM_ID) private platformId: Object) {
+    this.isBrowser = isPlatformBrowser(this.platformId);
+  }
+
+  ngOnInit() {
+    if (this.isBrowser) {
+      setTimeout(() => {
+        this.isCheckingAuth = false;
+      }, 50);
+    }
+  }
 }
