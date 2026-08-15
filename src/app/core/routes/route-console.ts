697b81dcafd37d653bc36d8881b23e8adbe90d21
SoniVega53 <svegam@miumg.edu.gt>
2026-08-14 21:21:42 -0600

    feat: creacion permisos roles

--- src/app/core/routes/route-console.ts
+++ src/app/core/routes/route-console.ts
@@ -4,6 +4,7 @@ import { EmpresaComponent } from '../../ui/console/empresa/empresa.component';
 import { GeneroComponent } from '../../ui/console/genero/genero.component';
 import { AccesoDenegadoComponent } from '../../ui/acceso-denegado/acceso-denegado.component';
 import { NotFoundComponent } from '../../ui/not-found/not-found.component';
+import { AsignacionOpcionRoleComponent } from '../../ui/console/asignacion-opcion-role/asignacion-opcion-role.component';
 
 export const CONSOLE_ROUTES: Routes = [
   {
@@ -18,6 +19,10 @@ export const CONSOLE_ROUTES: Routes = [
   {
     path: 'genero',
     component: GeneroComponent,
+  },
+  {
+    path: 'asignacion_opcion_role',
+    component: AsignacionOpcionRoleComponent,
   },
   {
     path: '403',
